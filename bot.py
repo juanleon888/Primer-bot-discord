a4b6eecc50527115e0d72be8c04569d6abe21383
juanleon888 <jluffyleon@gmail.com>
2025-01-27 18:18:55 -0500

    bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from bot_logic import*
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+bot.run("")
